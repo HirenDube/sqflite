8baf42f25a6a13eb96460add9ec008318d4c928d
HirenDube <heena.dube091@gmail.com>
2023-05-10 20:12:58 +0530

    Extended Image in Flutter (Image Editing widgte) Practice done 010/05/2023 (Simba)
    task tryign done

--- lib/Extended Image.dart
+++ lib/Extended Image.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:crud_pract_2nd_app/main.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+
 
 class BasicImageEditor extends StatefulWidget {
   const BasicImageEditor({Key? key}) : super(key: key);
@@ -9,27 +15,29 @@ class BasicImageEditor extends StatefulWidget {
   State<BasicImageEditor> createState() => _BasicImageEditorState();
 }
 
-class _BasicImageEditorState extends State<BasicImageEditor> with SingleTickerProviderStateMixin{
-  var _editorKey =
-  GlobalKey<ExtendedImageEditorState>();
+class _BasicImageEditorState extends State<BasicImageEditor>
+    with SingleTickerProviderStateMixin {
+  var controller = ScreenshotController();
+  Uint8List? imageBytes;
+
+  var _editorKey = GlobalKey<ExtendedImageEditorState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
-      buildAppBar(title: "Basic Image Editor App", bgColor: Colors.amber),
+          buildAppBar(title: "Basic Image Editor App", bgColor: Colors.amber),
       body: ExtendedImage.network(
         "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/Boruto-Orochimaru.jpg",
         mode: ExtendedImageMode.editor,
         extendedImageEditorKey: _editorKey,
         fit: BoxFit.contain,
-        initEditorConfigHandler: (state) =>
-            EditorConfig(
-                maxScale: 10.0,
-                hitTestSize: 30.0,
-                cropRectPadding: EdgeInsets.all(10.0),
-                lineColor: Colors.red,
-                cornerColor: Colors.amber),
+        initEditorConfigHandler: (state) => EditorConfig(
+            maxScale: 10.0,
+            hitTestSize: 30.0,
+            cropRectPadding: EdgeInsets.all(10.0),
+            lineColor: Colors.red,
+            cornerColor: Colors.amber),
       ),
       bottomNavigationBar: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
@@ -38,7 +46,7 @@ class _BasicImageEditorState extends State<BasicImageEditor> with SingleTickerPr
           children: [
             IconButton(
                 onPressed: () {
-                  if( _editorKey.currentState != null){
+                  if (_editorKey.currentState != null) {
                     _editorKey.currentState?.rotate(right: false);
                   }
                 },
@@ -49,6 +57,30 @@ class _BasicImageEditorState extends State<BasicImageEditor> with SingleTickerPr
             IconButton(
                 onPressed: () => _editorKey.currentState?.rotate(right: true),
                 icon: Icon(Icons.rotate_right)),
+            IconButton(
+                onPressed: () {
+                  final bytes = controller.captureFromWidget(Material(
+                      child: ExtendedImage.network(
+                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/Boruto-Orochimaru.jpg",
+                    mode: ExtendedImageMode.editor,
+                    extendedImageEditorKey: _editorKey,
+                    fit: BoxFit.contain,
+                    initEditorConfigHandler: (state) => EditorConfig(
+                        maxScale: 10.0,
+                        hitTestSize: 30.0,
+                        cropRectPadding: EdgeInsets.all(10.0),
+                        lineColor: Colors.red,
+                        cornerColor: Colors.amber),
+                  )));
+                  setState(() {
+                    this.imageBytes = bytes as Uint8List?;
+                  });
+
+                  final location = getApplicationDocumentsDirectory();
+                  final file = File("${location}/image.png");
+                  file.writeAsBytes(imageBytes!);
+                },
+                icon: Icon(Icons.save_alt))
           ],
         ),
       ),
